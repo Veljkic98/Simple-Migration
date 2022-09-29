c7d6a1b750398e9d41fe5af294d90805f43bce8f
Veljko Plecas <veljkicc110@gmail.com>
2022-09-29 13:55:57 +0200

    initialize project

---
+++ TransformService/src/test/java/com/transformservice/TransformServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transformservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransformServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
