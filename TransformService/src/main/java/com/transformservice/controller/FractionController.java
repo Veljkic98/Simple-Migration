fd086225e83f0c75b94e335c6ac31a7089650b75
Veljko Plecas <veljkicc110@gmail.com>
2022-10-14 10:46:00 +0200

    refactor

--- TransformService/src/main/java/com/transformservice/controller/FractionController.java
+++ TransformService/src/main/java/com/transformservice/controller/FractionController.java
@@ -1,12 +1,10 @@
 package com.transformservice.controller;
 
+import com.transformservice.controller.doc.FractionControllerSwagger;
 import com.transformservice.mapper.FractionMapper;
 import com.transformservice.domain.dto.FractionDto;
 import com.transformservice.domain.dto.FractionsDto;
 import com.transformservice.service.FractionService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,7 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "/profile/{profileId}/fraction")
-public class FractionController {
+public class FractionController implements FractionControllerSwagger {
 
     private final FractionService fractionService;
 
@@ -26,26 +24,16 @@ public class FractionController {
         this.fractionService = fractionService;
     }
 
+    @Override
     @GetMapping
-    @Operation(summary = "Get all Fractions by Profile Id")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Successful operation."),
-            @ApiResponse(responseCode = "404", description = "Profile not exists"),
-            @ApiResponse(responseCode = "500", description = "Internal Server Error")
-    })
     public ResponseEntity<List<FractionDto>> getAllByProfile(@PathVariable Long profileId) {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(FractionMapper.toDtoList(fractionService.getAllByProfile(profileId)));
     }
 
+    @Override
     @GetMapping(path = "/{fractionId}")
-    @Operation(summary = "Get one Fraction by Id")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Successful operation."),
-            @ApiResponse(responseCode = "404", description = "Fraction not found."),
-            @ApiResponse(responseCode = "500", description = "Internal Server Error")
-    })
     public ResponseEntity<FractionDto> getById(@PathVariable Long profileId,
                                                @PathVariable Long fractionId) {
         return ResponseEntity
@@ -53,14 +41,8 @@ public class FractionController {
                 .body(FractionMapper.toDto(fractionService.getById(profileId, fractionId)));
     }
 
+    @Override
     @PostMapping
-    @Operation(summary = "Save new Fraction")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "Successful operation."),
-            @ApiResponse(responseCode = "400", description = "Request body fields are invalid or missing"),
-            @ApiResponse(responseCode = "404", description = "Profile not found."),
-            @ApiResponse(responseCode = "500", description = "Internal Server Error")
-    })
     public ResponseEntity<List<FractionDto>> create(@PathVariable Long profileId,
                                                     @RequestBody @Validated FractionsDto fractionsDto) {
         return ResponseEntity
@@ -68,14 +50,8 @@ public class FractionController {
                 .body(FractionMapper.toDtoList(fractionService.create(profileId, fractionsDto)));
     }
 
+    @Override
     @PutMapping
-    @Operation(summary = "Update Fractions")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Successful operation."),
-            @ApiResponse(responseCode = "400", description = "Request body fields are invalid or missing"),
-            @ApiResponse(responseCode = "404", description = "Profile or Fraction not found."),
-            @ApiResponse(responseCode = "500", description = "Internal Server Error")
-    })
     public ResponseEntity<List<FractionDto>> update(@PathVariable Long profileId,
                                            @RequestBody FractionsDto fractionsDto) {
         return ResponseEntity
@@ -83,13 +59,8 @@ public class FractionController {
                 .body(FractionMapper.toDtoList(fractionService.update(profileId, fractionsDto)));
     }
 
+    @Override
     @DeleteMapping
-    @Operation(summary = "Delete Fractions")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "204", description = "Successful operation."),
-            @ApiResponse(responseCode = "404", description = "Profile or Fraction not found."),
-            @ApiResponse(responseCode = "500", description = "Internal Server Error")
-    })
     public ResponseEntity<Void> delete(@PathVariable Long profileId) {
         fractionService.delete(profileId);
 
