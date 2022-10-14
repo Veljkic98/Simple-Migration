85463c9fe844d2e62c7a4495869da31d1f559521
Veljko Plecas <veljkicc110@gmail.com>
2022-10-14 10:14:56 +0200

    add swagger and doc for fraction

--- TransformService/src/main/java/com/transformservice/controller/FractionController.java
+++ TransformService/src/main/java/com/transformservice/controller/FractionController.java
@@ -4,6 +4,9 @@ import com.transformservice.mapper.FractionMapper;
 import com.transformservice.domain.dto.FractionDto;
 import com.transformservice.domain.dto.FractionsDto;
 import com.transformservice.service.FractionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,6 +27,12 @@ public class FractionController {
     }
 
     @GetMapping
+    @Operation(summary = "Get all Fractions by Profile Id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation."),
+            @ApiResponse(responseCode = "404", description = "Profile not exists"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
     public ResponseEntity<List<FractionDto>> getAllByProfile(@PathVariable Long profileId) {
         return ResponseEntity
                 .status(HttpStatus.OK)
@@ -31,6 +40,12 @@ public class FractionController {
     }
 
     @GetMapping(path = "/{fractionId}")
+    @Operation(summary = "Get one Fraction by Id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation."),
+            @ApiResponse(responseCode = "404", description = "Fraction not found."),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
     public ResponseEntity<FractionDto> getById(@PathVariable Long profileId,
                                                @PathVariable Long fractionId) {
         return ResponseEntity
@@ -39,6 +54,13 @@ public class FractionController {
     }
 
     @PostMapping
+    @Operation(summary = "Save new Fraction")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Successful operation."),
+            @ApiResponse(responseCode = "400", description = "Request body fields are invalid or missing"),
+            @ApiResponse(responseCode = "404", description = "Profile not found."),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
     public ResponseEntity<List<FractionDto>> create(@PathVariable Long profileId,
                                                     @RequestBody @Validated FractionsDto fractionsDto) {
         return ResponseEntity
@@ -47,6 +69,13 @@ public class FractionController {
     }
 
     @PutMapping
+    @Operation(summary = "Update Fractions")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation."),
+            @ApiResponse(responseCode = "400", description = "Request body fields are invalid or missing"),
+            @ApiResponse(responseCode = "404", description = "Profile or Fraction not found."),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
     public ResponseEntity<List<FractionDto>> update(@PathVariable Long profileId,
                                            @RequestBody FractionsDto fractionsDto) {
         return ResponseEntity
@@ -55,6 +84,12 @@ public class FractionController {
     }
 
     @DeleteMapping
+    @Operation(summary = "Delete Fractions")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Successful operation."),
+            @ApiResponse(responseCode = "404", description = "Profile or Fraction not found."),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
     public ResponseEntity<Void> delete(@PathVariable Long profileId) {
         fractionService.delete(profileId);
 
